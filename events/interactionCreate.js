3eac61688ca0d12df14d50dbd3ef9ca15ce41a40
Toffi <Stefanie.Reuter98@t-online.de>
2025-01-16 21:33:48 +0100

    Use ensureUserAndGuild to check if relevant data is in database

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,6 @@
+// to use global functions
+const functions = require('../functions.js');
+
 module.exports = {
 	name: "interactionCreate",
 	once: false,
@@ -7,8 +10,13 @@ module.exports = {
 
         if(!interaction.isCommand()) return;
 
+        // Überprüfung, ob Guild, Member und User in der Datenbank sind, bei Bedarf einfügen
+        await functions.ensureUserAndGuild(interaction.user.id, interaction.guild.id);
+
+        // Befehl holen
         const command = client.commands.get(interaction.commandName);
 
+        // Befehl ausführen
         if(command) {
             try {
                 await command.execute(interaction);
@@ -16,9 +24,9 @@ module.exports = {
                 console.error(error);
 
                 if(interaction.deferred || interaction.replied) {
-                    interaction.editReply("> ⚠️ Es ist ein Fehler beim Ausführen des Befehls aufgetreten!");
+                    interaction.editReply("⚠️ Es ist ein Fehler beim Ausführen des Befehls aufgetreten!");
                 } else {
-                    interaction.reply("> ⚠️ Es ist ein Fehler beim Ausführen des Befehls aufgetreten!");
+                    interaction.reply("⚠️ Es ist ein Fehler beim Ausführen des Befehls aufgetreten!");
                 }
             }
         }
